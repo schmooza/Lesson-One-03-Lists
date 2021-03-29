3c4d72616ca23d8094f144500fcad7b0263bf99b
TsukubaIS <subscriptions@tis.ac.jp>
2021-03-29 22:42:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from myCode import *
+
+
+# runs mycode like a conductor in an orchestra
+colors()
+listoMatic()
+
+# test
+
---
+++ myCode.py
@@ -0,0 +1,42 @@
+import random
+from colored import fg, bg, attr
+
+
+
+def colors ():
+	# change the colors
+	print ('%s%s ListoMatic !!! %s' % (fg('white'), bg('yellow'), attr('reset')))
+	print("\n")
+
+
+
+def listoMatic():
+	# comment
+	myList = []
+
+
+	# comment
+	x = 10
+
+
+	# comment
+	while x > 0:
+		# comment
+		randomNum = random.randint(1,2)
+		
+		# comment
+		myList.append(randomNum)
+
+		# comment
+		x = x - 1
+		print(x, "debug")
+	
+	print("\n")
+	print (myList, "%s%s The list of random numbers %s" % (fg('green'), bg('purple_3'), attr('reset')))
+
+	lengthIs = len(myList) 
+
+	
+	print("\n")
+	print(f'There are {lengthIs} hats in the shop.')
+
